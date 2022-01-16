5c32908ce2d37efef0e4f5e68f5d7df58f79af07
m3h3d1 <hasanmehedi185@gmail.com>
2022-01-16 14:05:38 +0600

    initial

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop; // number of pixels the content of a <div> element is scrolled horizontally and vertically
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active");
+    document.querySelector(".navbar").classList.toggle("show");
+  })
+})
+
+//Hamburger
+
+const hamBurger = document.querySelector(".hamburger");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("show");
+})
+
+//Footer Year 
+var year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
